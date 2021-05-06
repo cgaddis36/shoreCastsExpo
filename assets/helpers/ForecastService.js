96ae5c4a5fd675c10bf600fe0cceacd287162eec
Chase.Gaddis <cgaddis36@gmail.com>
2021-05-06 06:40:44 -0500

    api update

--- assets/helpers/ForecastService.js
+++ assets/helpers/ForecastService.js
@@ -1,26 +1,26 @@
 const rootURL = "http://www.shorecasts.com/graphql"
 
 export default async function forecastService(zipcode) {
-     try {
-       let response = await fetch(`${rootURL}`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        query: `
-        query{
-          closestStation(zip: "${zipcode}"){
-            id
-            stationId
-            location
-          }}`,
-        }),
-      });
-      let responseJSON = await response.json();
-        console.log("your data", responseJSON);
-        return responseJSON;
-      } catch (error) {
-        console.error(error)
-      }
-    }
+  try {
+   let response = await fetch(`${rootURL}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: `
+      query{
+        closestStation(zip: "${zipcode}"){
+          id
+          stationId
+          location
+        }}`,
+      }),
+    });
+    let responseJSON = await response.json();
+      console.log("your data", responseJSON);
+    return responseJSON;
+    } catch (error) {
+      console.error(error)
+  }
+}
