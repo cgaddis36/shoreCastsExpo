7dc11e279b3180c2ea83ed1d02472062824e060b
Chase.Gaddis <cgaddis36@gmail.com>
2021-06-08 09:37:05 -0500

    add waterTempData from NOAA api call to state params

--- assets/screens/Loading.js
+++ assets/screens/Loading.js
@@ -41,28 +41,33 @@ function Loading() {
                               fetch(foreData.properties.forecast)
                               .then(response => {
                                 response.json().then((forecastSummary) => {
-                                  
-                                  var tidingsToday = []
-                                  var tidingsTomorrow = []
-                                  tideData["predictions"].forEach(function(tide) {
-                                    if(tide["t"].slice(0, 10).replace(/[-]/g,'') == route.params.beginDate){
-                                      tidingsToday.push(tide["v"])
-                                    }
-                                    else {
-                                      tidingsTomorrow.push(tide["v"])
-                                    }
-                                  })
-                                  navigation.navigate("Forecast", {
-                                    tidesToday: tidingsToday,
-                                    tidesTomorrow: tidingsTomorrow,
-                                    loading: false,
-                                    forecastData: forecastingData,
-                                    timeLabels: ["1am", "2am", "3am", "4am", "5am", "6am",
-                                    "7am", "8am", "9am", "10am", "11am", "12pm",
-                                    "1pm", "2pm", "3pm", "4pm", "5pm", "6pm",
-                                    "7pm", "8pm", "9pm", "10pm", "11pm"],
-                                    forecastSummary: forecastSummary
-                                  })
+                                  fetch((`https://api.tidesandcurrents.noaa.gov/api/prod/datagetter?begin_date=${route.params.beginDate}&end_date=${route.params.endDate}&station=${data.data.closestStation.stationId}&product=water_temperature&units=english&time_zone=gmt&application=ports_screen&datum=MLLW&format=json`))
+                                    .then(response => {
+                                      response.json().then((waterTempData) => {
+                                            var tidingsToday = []
+                                            var tidingsTomorrow = []
+                                            tideData["predictions"].forEach(function(tide) {
+                                              if(tide["t"].slice(0, 10).replace(/[-]/g,'') == route.params.beginDate){
+                                                tidingsToday.push(tide["v"])
+                                              }
+                                              else {
+                                                tidingsTomorrow.push(tide["v"])
+                                              }
+                                            })
+                                            navigation.navigate("Forecast", {
+                                              tidesToday: tidingsToday,
+                                              tidesTomorrow: tidingsTomorrow,
+                                              loading: false,
+                                              forecastData: forecastingData,
+                                              timeLabels: ["1am", "2am", "3am", "4am", "5am", "6am",
+                                              "7am", "8am", "9am", "10am", "11am", "12pm",
+                                              "1pm", "2pm", "3pm", "4pm", "5pm", "6pm",
+                                              "7pm", "8pm", "9pm", "10pm", "11pm"],
+                                              forecastSummary: forecastSummary,
+                                              waterTempData: waterTempData
+                                            })
+                                          })
+                                        })
                                 })
                               })
                             })
