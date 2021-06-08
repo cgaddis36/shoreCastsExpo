da38448d79061adb4bd02ca5f0597d7e0b77c6f8
Chase.Gaddis <cgaddis36@gmail.com>
2021-06-08 13:27:47 -0500

    create container for breaking out hourly forecast

---
+++ assets/components/containers/ForecastContainer.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react';
+import { ImageBackground, StyleSheet, View, Image, Text, Button } from 'react-native';
+import { useFocusEffect,
+         useNavigation,
+         useRoute,
+         useNavigationState } from '@react-navigation/native';
+function ForecastContainer({image}) {
+  const route = useRoute();
+  const navigation = useNavigation();
+  const state = useNavigationState(state => state);
+  console.log(image)
+  return (
+    <View style={{height:130,width:130, marginLeft: 10, borderWidth: 0.5}}>
+      <View style={{flex:2}}>
+        <Image source={{uri: image}}
+               style={{ flex: 1, width: null, height: null, resizeMode: 'cover'}}
+          />
+      </View>
+      <View style={{flex:1, paddingLeft: 2.5, paddingTop: 5, paddingRight: 2.5}}>
+        <Text>
+          WORK
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default ForecastContainer;
