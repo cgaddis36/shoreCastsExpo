0dc05a647d4d4286a8e9704e09608fb0b11b87e6
Chase.Gaddis <cgaddis36@gmail.com>
2021-06-11 10:25:12 -0500

    remove console.log and unnecessary css change styling component names for conventional purposes

--- assets/screens/Forecast.js
+++ assets/screens/Forecast.js
@@ -18,8 +18,6 @@ function Forecast() {
   const route = useRoute();
   const navigation = useNavigation();
   const state = useNavigationState(state => state);
-    console.log("FORECAST SUMMARY", route.params.forecastSummaryData)
-    // console.log("Water Temp Tmorrow Hourly params", route.params.forecastSummary)
 
   const lineDataToday = {
     labels: route.params.timeLabels,
@@ -101,10 +99,10 @@ function Forecast() {
     </ScrollView>
     <View style={styles.charts}>
       <View style={styles.navContainer}>
-        <Text style={styles.logoText}>Water Temperature: {Math.min.apply(Math, route.params.waterTempToday)} - {Math.max.apply(Math, route.params.waterTempToday)}{"\u00b0"+"F"}</Text>
+        <Text style={styles.bannerText}>Water Temperature: {Math.min.apply(Math, route.params.waterTempToday)} - {Math.max.apply(Math, route.params.waterTempToday)}{"\u00b0"+"F"}</Text>
       </View>
       <View style={styles.navContainer}>
-        <Text style={styles.logoText}>Tide Predictions for {route.params.beginDate}</Text>
+        <Text style={styles.bannerText}>Tide Predictions for {route.params.beginDate}</Text>
       </View>
       <ScrollView horizontal={true}
         showsHorizontalScrollIndicator={false}
@@ -172,12 +170,6 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: "rgb(30, 94, 238)"
   },
-  tideText: {
-
-  },
-  hourlyForecastText: {
-    color: 'white'
-  },
   navContainer: {
     marginTop: 5,
     borderRadius:5,
@@ -196,24 +188,11 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center'
   },
-  navBar: {
-    marginTop: 75,
-    position: 'absolute',
-    top: 55,
-    flexDirection: "row",
-    marginBottom: 200,
-    height: 50,
-    width: "90%",
-    justifyContent: 'space-around',
-  },
   charts: {
     marginTop: 150,
     position: 'absolute',
-    // top: 55,
     flexDirection: "column",
     marginBottom: 200,
-    // height: 50,
-    // width: "90%",
     justifyContent: 'space-around',
   },
   forecastContainer:{
@@ -226,26 +205,15 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     borderWidth: 1,
     },
-  logoContainer:{
-    marginTop: 10,
-    position: 'absolute',
-    top: 55,
-    alignItems: 'center',
-    width: 200,
-    height: 38,
-    backgroundColor: "rgba(165, 168, 176, 0.64)",
-    borderRadius: 10,
-    borderWidth: 1,
-    },
-    logoText:{
+    bannerText:{
       fontSize: 15,
       color: 'white',
       textAlign: 'center'
     },
-    loadingText:{
-      fontSize: 30,
-      marginBottom: 500
-    }
+    // loadingText:{
+    //   fontSize: 30,
+    //   marginBottom: 500
+    // }
 });
 
 export default Forecast;
