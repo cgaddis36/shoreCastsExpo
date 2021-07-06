379a106fe537f75ac5e1f4d4c4004a7d5c348985
Chase.Gaddis <cgaddis36@gmail.com>
2021-07-06 13:17:03 -0500

    button0 component

---
+++ assets/components/buttons/Button0.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StyleSheet, View, Button } from 'react-native';
+
+function Button0({title, presser}) {
+  return (
+    <View style={styles.button}>
+      <Button
+      alignSelf='center'
+      title={title}
+      color='white'
+      onPress={() => presser()
+        }
+      />
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: "40%",
+    height: 40,
+    borderRadius:20,
+    borderWidth: 1,
+    backgroundColor: "rgba(51, 52, 56, 0.64)",
+    marginBottom: 50
+   },
+});
+
+export default Button0;
